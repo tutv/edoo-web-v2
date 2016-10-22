52fbf2687fcf114997a828b30b5378d38e974848
Tu TV <tutv95@gmail.com>
2016-10-22 17:06:45 +0700

    Updated class details

--- src/app/components/class-details/class-details.component.ts
+++ src/app/components/class-details/class-details.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {ClassService} from "../../services/class.service";
+import {Transition, UIRouter} from "ui-router-ng2";
+import {Input} from "@angular/core/src/metadata/directives";
 
 @Component({
     selector: 'app-class-details',
@@ -6,8 +9,14 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./class-details.component.css']
 })
 export class ClassDetailsComponent implements OnInit {
+    @Input() classDetails;
+    public class_id;
 
-    constructor() {
+    constructor(private router: UIRouter,
+                private transition: Transition) {
+        this.class_id = transition.params()['classId'];
+
+        console.log(this.class_id);
     }
 
     ngOnInit() {
@@ -18,5 +27,16 @@ export class ClassDetailsComponent implements OnInit {
 export const classDetailsState = {
     name: 'class.details',
     url: '/:classId',
-    component: ClassDetailsComponent
+    component: ClassDetailsComponent,
+    resolve: [
+        {
+            token: 'classDetails',
+            deps: [Transition, ClassService],
+            resolveFn: (trans, classSvc) => {
+                var classId = trans.params().classId;
+
+                return classSvc.getListClasses();
+            }
+        }
+    ]
 };
