22573ff0e1e6d575fcab3178c69ebdf4ec77aeca
quytm <quytm_58@vnu.edu.vn>
2016-10-26 22:11:46 +0700

    PostDetail: Thêm chức năng vote, devote, delete post

--- src/app/components/post-details/post-details.component.ts
+++ src/app/components/post-details/post-details.component.ts
@@ -1,18 +1,23 @@
 import {Component, OnInit} from '@angular/core';
-import {Transition} from "ui-router-ng2";
+import {Transition, UIRouter} from "ui-router-ng2";
 import {ClassService} from "../../services/class.service";
 import {Input} from "@angular/core/src/metadata/directives";
+import {PostService} from "../../services/post.service";
 
 @Component({
     selector: 'app-post-details',
     templateUrl: './post-details.component.html',
-    styleUrls: ['./post-details.component.css']
+    styleUrls: ['./post-details.component.css'],
+    providers: [PostService]
 })
 export class PostDetailsComponent implements OnInit {
     @Input() post;
     @Input() listClasses;
 
-    constructor() {
+    constructor(
+        private postService: PostService,
+        private router: UIRouter
+    ) {
     }
 
     ngOnInit() {
@@ -20,6 +25,31 @@ export class PostDetailsComponent implements OnInit {
 
     public onComment(comment) {
         this.post.comments.push(comment);
+    }
+
+    public votePost(post_id, content) {
+        this.postService.votePost(post_id, content)
+            .then(data => {
+                this.post.vote_count = data.vote_count;
+            })
+            .catch(error => {
+                window.alert('Bạn không thể đánh giá cho bài viết này nữa! ' + error);
+            });
+    }
+
+    public deletePost(post_id) {
+        if (confirm("Xóa bài viết?") == false) {
+            return;
+        }
+
+        this.postService.deletePost(post_id)
+            .then(() => {
+                window.alert('Xóa bài viết thành công!');
+                this.router.stateService.go('classes');
+            })
+            .catch(error => {
+                console.log('Error: ' + error);
+            });
     }
 
 }
@@ -35,6 +65,7 @@ export const postDetailsState = {
             resolveFn: (trans, classSvc) => {
                 var postId = trans.params().postId;
 
+                console.log('router get PostDetails');
                 return classSvc.getPost(postId);
             }
         },
@@ -43,8 +74,10 @@ export const postDetailsState = {
             deps: [ClassService],
             resolveFn: (classSvc) => {
 
+                console.log('router get ListClass');
+
                 return classSvc.getListClasses();
-            }
+            },
         }
     ]
 };
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -26,4 +26,49 @@ export class PostService {
             .toPromise();
     }
 
+    /**
+     * Vote Post
+     * @param post_id
+     * @param content:
+     *          1: vote,     0: cancel vote/devote,      -1: devote
+     */
+    public votePost(post_id, content){
+        var data = `post_id=${post_id}&content=${content}`;
+
+        var args = {
+            method: 'POST',
+            url: '/votepost',
+            data: data
+        };
+
+        return this.api
+            .requestAuth(args)
+            .map(response => {
+                var data = response.json();
+
+                return data.data;
+            })
+            .toPromise();
+    }
+
+
+    public deletePost(post_id){
+        var data = `post_id=${post_id}`;
+
+        var args = {
+            method: 'POST',
+            url: '/deletepost',
+            data: data
+        };
+
+        return this.api
+            .requestAuth(args)
+            // .map(respose => {
+            //     var data = respose.json();
+            //
+            //     return data.data;
+            // })
+            .toPromise();
+    }
+
 }
